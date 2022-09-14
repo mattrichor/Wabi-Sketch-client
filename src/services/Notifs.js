f05783ee75ebc31aeb83fefccdc359e46e60dc18
mattrichor <matt.a.geyer@gmail.com>
2022-09-13 21:00:10 -0400

    more stuff

--- src/services/Notifs.js
+++ src/services/Notifs.js
@@ -1,4 +1,4 @@
-import Client from './Api'
+import Client from './api'
 
 export const GetNotifs = async (userId) => {
   try {
--- src/services/Prompts.js
+++ src/services/Prompts.js
@@ -1,4 +1,4 @@
-import Client from './Api'
+import Client from './api'
 export const GetPromptByDate = async (date) => {
   try {
     const res = await Client.get(`/prompts/${date}`)
